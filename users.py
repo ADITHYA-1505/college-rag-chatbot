ad186ec3a667e5d271a0b045a3fe1a5f34dd2251
ADITHYA-1505 <gnsadithya2005@gmail.com>
2025-12-27 11:43:18 +0530

    Add files via upload

---
+++ users.py
@@ -0,0 +1,14 @@
+USERS = {
+    "student_cse": {
+        "role": "student",
+        "department": "CSE"
+    },
+    "faculty_cse": {
+        "role": "faculty",
+        "department": "CSE"
+    },
+    "admin": {
+        "role": "admin",
+        "department": "ALL"
+    }
+}
